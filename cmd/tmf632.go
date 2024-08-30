aace628c99c68a9dae69fd6a14c3644764ee7bca
chailuck <chailuck@gmail.com>
2024-08-30 21:35:37 +0700

    chore: Add main function and database initialization
    This commit adds the main function to the code, which initializes the database connection using the NewDB() function from the database package. It also includes logging statements for tracing the server startup process.

---
+++ cmd/tmf632.go
@@ -0,0 +1,20 @@
+
+func main() {
+	//ctx := context.Background()
+	log.AppTraceLog.Info(log.AppTraceLogInfo("START SERVER", "ADMIN", "", "", "", ""))
+
+	db, err := database.NewDB()
+	if err != nil {
+		log.AppTraceLog.Error(log.AppTraceLogInfo("START SERVER", "ADMIN", "", "", "", err.Error()))
+		return
+	}
+
+	// Create service
+
+
+	log.AppTraceLog.Info(log.AppTraceLogInfo("Listening on port :"+apiPort+"...", "ADMIN", "", "", "", ""))
+
+	log.AppTraceLog.Error(log.AppTraceLogInfo("Port :8080...", "ADMIN", "", "", "", http.ListenAndServe(":8080", httpHandler).Error()))
+
+	//log.Fatal(http.ListenAndServe(":8080", httpHandler))
+}
