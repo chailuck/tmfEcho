30a23ef2662259143d2fce30032935261f83240e
chailuck <chailuck@gmail.com>
2024-08-31 07:23:49 +0700

    chore: Update import paths and remove unused code
    This commit updates the import paths in multiple files to use the new package structure of "tmfEcho/internal" instead of "GOKIT_v001/internal". It also removes unused code and refactors the main function for better readability and maintainability.

--- cmd/tmf632.go
+++ cmd/tmf632.go
@@ -1,20 +1,37 @@
-package mains
+package main
 
 import (
-	//	"tmfEcho/internal/database"
+	"tmfEcho/internal/database"
 	"tmfEcho/internal/log"
+	"tmfEcho/pkg/party"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
 	//ctx := context.Background()
 	log.AppTraceLog.Info(log.AppTraceLogInfo("START SERVER", "ADMIN", "", "", "", ""))
-	/*
-		db, err := database.NewDB()
-		if err != nil {
-			log.AppTraceLog.Error(log.AppTraceLogInfo("START SERVER", "ADMIN", "", "", "", err.Error()))
-			return
-		}
+
+	db, err := database.NewDB()
+	if err != nil {
+		log.AppTraceLog.Error(log.AppTraceLogInfo("START SERVER", "ADMIN", "", "", "", err.Error()))
+		return
+	}
+
+	e := echo.New()
+
+	h := party.PartyHandler{}
+
+	h.Initialize(db)
+
+	e.GET("/individual", h.GetIndividual)
+	/*	e.POST("/customers", h.SaveCustomer)
+		e.GET("/customers/:id", h.GetCustomer)
+		e.PUT("/customers/:id", h.UpdateCustomer)
+		e.DELETE("/customers/:id", h.DeleteCustomer)
 	*/
+	e.Logger.Fatal(e.Start(":8080"))
+
 	// Create service
 
 	//log.AppTraceLog.Info(log.AppTraceLogInfo("Listening on port :"+apiPort+"...", "ADMIN", "", "", "", ""))
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/godror/godror v0.44.4
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
@@ -15,9 +16,18 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/godror/knownpb v0.1.2 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
